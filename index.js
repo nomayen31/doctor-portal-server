afd17e7b4ebea28559c81f88f7e82d218475cd68
nomayen <nomayen.ohin@gmail.com>
2023-03-08 05:12:57 +0600

    jwt token

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express')
 var cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { response } = require('express');
 require('dotenv').config();
 const port =process.env.PORT || 5000;
 
 var app = express()
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -67,13 +71,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
            
            const result =await bookingsCollection.insertOne(booking)
            res.send(result);
-        })
+        });
 
-        app.get('/users', async(req, res)=>{
-            const user = req.body;
-            const result = await usersCollection.insertOne(user);
-            res.send(result)
+        app.get('/jst', async(req, res)=>{
+            const email =req.email.email;
+            const query ={email: email}
+            const user =await usersCollection.findOne(query)
+            if (user) {
+                const token =jwt.sign({email}, process.env.ACESS_TOKEN,{expiresIn: '1h'})
+              return res.send({accessToken:token})
+            }
+            console.log(user);  
+            res.status(403).send({accessToken:''})
         })
+      
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
     }
     finally{
 
